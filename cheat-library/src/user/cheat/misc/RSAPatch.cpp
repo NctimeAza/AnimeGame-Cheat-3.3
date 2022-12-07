fca923fc9ba7fba2538dcc7a4cf5294fb5f874ad
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2022-12-06 20:35:14 -0700

    Interactive Map

--- cheat-library/src/user/cheat/misc/RSAPatch.cpp
+++ cheat-library/src/user/cheat/misc/RSAPatch.cpp
@@ -45,7 +45,7 @@ namespace cheat::feature
 		auto key = il2cppi_to_string(xmlString);
 		auto newKey = string_to_il2cppi("<RSAKeyValue><Modulus>xbbx2m1feHyrQ7jP+8mtDF/pyYLrJWKWAdEv3wZrOtjOZzeLGPzsmkcgncgoRhX4dT+1itSMR9j9m0/OwsH2UoF6U32LxCOQWQD1AMgIZjAkJeJvFTrtn8fMQ1701CkbaLTVIjRMlTw8kNXvNA/A9UatoiDmi4TFG6mrxTKZpIcTInvPEpkK2A7Qsp1E4skFK8jmysy7uRhMaYHtPTsBvxP0zn3lhKB3W+HTqpneewXWHjCDfL7Nbby91jbz5EKPZXWLuhXIvR1Cu4tiruorwXJxmXaP1HQZonytECNU/UOzP6GNLdq0eFDE4b04Wjp396551G99YiFP2nqHVJ5OMQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>");
 
-		if (rsaPatch.f_Enabled->enabled() && key.rfind("<RSAKeyValue><Modulus>15RBm/vARY0axYksImhsTicpv09OYfS4+wCvmE7") != std::string::npos) // 2nd time fn is called
+		if (rsaPatch.f_Enabled->enabled() && (key.rfind("<RSAKeyValue><Modulus>15RBm/vARY0axYksImhsTicpv09OYfS4+wCvmE7") != std::string::npos || key.rfind("<RSAKeyValue><Modulus>lCwdYrveozYYcKOSz4cjBfORvd6POZSxsM9JybW") != std::string::npos)) // 2nd time fn is called
 			memcpy(xmlString, newKey, newKey->fields.length * 2);
 
 		CALL_ORIGIN(RSA_FromXmlString_Hook, __this, xmlString, method);
