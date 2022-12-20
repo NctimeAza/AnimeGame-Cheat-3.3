d63ee58922fbaf708d930c72569d9cfa52b95267
xTaiwanPingLord <tojimmy0917@gmail.com>
2022-12-20 16:12:36 +0800

    Add option to use custom RSA Private Key

--- cheat-library/src/user/cheat/misc/RSAPatch.cpp
+++ cheat-library/src/user/cheat/misc/RSAPatch.cpp
@@ -4,15 +4,17 @@
 
 namespace cheat::feature
 {
-	static void RSA_FromXmlString_Hook(app::RSA* __this, app::String* xmlString, MethodInfo* method);
+	static void RSA_FromXmlString_Hook(app::RSA *__this, app::String *xmlString, MethodInfo *method);
 
 	RSAPatch::RSAPatch() : Feature(),
-		NFP(f_Enabled, "General::RSAPatch", "RSA Patch", false)
+		NFP(f_Enabled, "General::RSAPatch", "RSA Patch", false),
+		NF(f_PublicKey, "General::RSAPatch", false),
+		NF(f_PrivateKey, "General::RSAPatch", false)
 	{
 		HookManager::install(app::RSA_FromXmlString, RSA_FromXmlString_Hook);
 	}
 
-	const FeatureGUIInfo& RSAPatch::GetGUIInfo() const
+	const FeatureGUIInfo &RSAPatch::GetGUIInfo() const
 	{
 		TRANSLATED_MODULE_INFO("Settings");
 		return info;
@@ -21,6 +23,10 @@ namespace cheat::feature
 	void RSAPatch::DrawMain()
 	{
 		ConfigWidget(_TR("RSA Patch"), f_Enabled, _TR("Runtime patcher. Only use it on private servers."));
+		ConfigWidget(_TR("Custom Public Key"), f_PublicKey, _TR("Dress Texture.\n" \
+			"Example path: C:\\Dress.png"));
+		ConfigWidget(_TR("Custom Private Key"), f_PrivateKey, _TR("Glider Texture.\n" \
+			"Example path: C:\\Glider.png"));
 	}
 
 	bool RSAPatch::NeedStatusDraw() const
@@ -31,24 +37,58 @@ namespace cheat::feature
 	void RSAPatch::DrawStatus()
 	{
 		ImGui::Text(_TR("RSA Patch"));
+		// to be done
 	}
 
-	RSAPatch& RSAPatch::GetInstance()
+	RSAPatch &RSAPatch::GetInstance()
 	{
 		static RSAPatch instance;
 		return instance;
 	}
 
-	static void RSA_FromXmlString_Hook(app::RSA* __this, app::String* xmlString, MethodInfo* method)
+	static void RSA_FromXmlString_Hook(app::RSA *__this, app::String *xmlString, MethodInfo *method)
 	{
-		auto& rsaPatch = RSAPatch::GetInstance();
+		auto &rsaPatch = RSAPatch::GetInstance();
 		auto key = il2cppi_to_string(xmlString);
-		auto newKey = string_to_il2cppi("<RSAKeyValue><Modulus>xbbx2m1feHyrQ7jP+8mtDF/pyYLrJWKWAdEv3wZrOtjOZzeLGPzsmkcgncgoRhX4dT+1itSMR9j9m0/OwsH2UoF6U32LxCOQWQD1AMgIZjAkJeJvFTrtn8fMQ1701CkbaLTVIjRMlTw8kNXvNA/A9UatoiDmi4TFG6mrxTKZpIcTInvPEpkK2A7Qsp1E4skFK8jmysy7uRhMaYHtPTsBvxP0zn3lhKB3W+HTqpneewXWHjCDfL7Nbby91jbz5EKPZXWLuhXIvR1Cu4tiruorwXJxmXaP1HQZonytECNU/UOzP6GNLdq0eFDE4b04Wjp396551G99YiFP2nqHVJ5OMQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>");
 
-		if (rsaPatch.f_Enabled->enabled() && (key.rfind("<RSAKeyValue><Modulus>15RBm/vARY0axYksImhsTicpv09OYfS4+wCvmE7") != std::string::npos || key.rfind("<RSAKeyValue><Modulus>lCwdYrveozYYcKOSz4cjBfORvd6POZSxsM9JybW") != std::string::npos)) // 2nd time fn is called
-			memcpy(xmlString, newKey, newKey->fields.length * 2);
+		app::String *public_key;
+		app::String *private_key;
 
+		if (rsaPatch.f_Enabled->enabled())
+		{
+			if (rsaPatch.f_PublicKey->enabled())
+				public_key = string_to_il2cppi(rsaPatch.f_PublicKey->value());
+			else
+				public_key = string_to_il2cppi("<RSAKeyValue><Modulus>xbbx2m1feHyrQ7jP+8mtDF/pyYLrJWKWAdEv3wZrOtjOZzeLGPzsmkcgncgoRhX4dT+1itSMR9j9m0/OwsH2UoF6U32LxCOQWQD1AMgIZjAkJeJvFTrtn8fMQ1701CkbaLTVIjRMlTw8kNXvNA/A9UatoiDmi4TFG6mrxTKZpIcTInvPEpkK2A7Qsp1E4skFK8jmysy7uRhMaYHtPTsBvxP0zn3lhKB3W+HTqpneewXWHjCDfL7Nbby91jbz5EKPZXWLuhXIvR1Cu4tiruorwXJxmXaP1HQZonytECNU/UOzP6GNLdq0eFDE4b04Wjp396551G99YiFP2nqHVJ5OMQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>");
+
+			// Private key contains: <P> <Q> <DP> <DQ> <D> <Exponent> <Modulus> <InverseQ>
+			// Public key contains: <Exponent> <Modulus>
+
+			LOG_WARNING("Get RSA Key:");
+			LOG_WARNING(key.c_str());
+
+			// Public key
+			if ((key.rfind("<RSAKeyValue><Modulus>15RBm/vARY0axYksImhsTicpv09OYfS4+wCvmE7") != std::string::npos || key.rfind("<RSAKeyValue><Modulus>lCwdYrveozYYcKOSz4cjBfORvd6POZSxsM9JybW") != std::string::npos)) // 2nd time fn is called
+			{
+				LOG_WARNING("Patching Public Key");
+				LOG_WARNING(key.c_str());
+				memcpy(xmlString, public_key, public_key->fields.length * 2);
+			}
+
+			// Private key: no need to use on grasscutter
+			if (rsaPatch.f_PrivateKey->enabled())
+			{
+				private_key = string_to_il2cppi(rsaPatch.f_PrivateKey->value());
+
+				if (key.find("</DP>") != std::string::npos && key.rfind("</InverseQ>") != std::string::npos)
+				{
+					LOG_WARNING("Patching Private Key");
+					LOG_WARNING(key.c_str());
+					memcpy(xmlString, private_key, private_key->fields.length * 2);
+				}
+			}
+		}
 		CALL_ORIGIN(RSA_FromXmlString_Hook, __this, xmlString, method);
 	}
 
-}
+	}
