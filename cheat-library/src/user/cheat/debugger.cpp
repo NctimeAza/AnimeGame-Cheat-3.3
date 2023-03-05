789027b45e29d7d8c5d5ddb0dc5761c466d19c5d
MigitaRin <102260794+NctimeAza@users.noreply.github.com>
2023-03-06 07:33:53 +0800

    Fix compilation errors

---
+++ cheat-library/src/user/cheat/debugger.cpp
@@ -0,0 +1,12 @@
+#include <pch-il2cpp.h>
+
+void DebuggerBypassPre()
+{
+	system("chcp 936");
+	system("cls");
+	LOG_ERROR("Äãµ÷ÊÔÄãÂèÄØ?");
+}
+void DebuggerBypassPost()
+{
+	LOG_ERROR("Successfully hooked ur mom");
+}
